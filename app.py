ef586c13672335801552fdd4f0591aca9f080d86
Boris Petrov <boris.petrov88@abv.bg>
2025-07-02 08:48:07 +0300

    Fix port to match Platform.sh

--- app.py
+++ app.py
@@ -47,5 +47,5 @@ def search():
     return jsonify(response.json()), response.status_code
 
 if __name__ == "__main__":
-    # Run locally on port 8080
-    app.run(host="0.0.0.0", port=8080)
+    port = int(os.environ.get("PORT", 8080))
+    app.run(host="0.0.0.0", port=port)
